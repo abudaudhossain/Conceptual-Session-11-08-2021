6d1fbf5b2ad31f761926e25cacbf44a4c375f042
abudaudhossain <abudaudhossainsumon@gmail.com>
2021-08-11 20:05:41 +0600

    add rest product details function

--- problem-1.js
+++ problem-1.js
@@ -1,26 +1,41 @@
-var protucts = [
+var products = [
     {
         productName: 'Loptop',
         productId: 01,
         productStock: 199
-
     },
     {
         productName: 'teblet',
         productId: 01,
         productStock: 199
-
     },
     {
         productName: 'wacth',
         productId: 01,
         productStock: 199
-
+    },
+    {
+        productName: 'Loptop',
+        productId: 01,
+        productStock: 199
     },
     {
         productName: 'phone',
         productId: 01,
         productStock: 199
-
     }
 ]
+
+function getRestProduct(itemName){
+    let restProducts =[];
+    for(let product of products){
+        // console.log(product);
+        if(product['productName'] == itemName){
+            continue;
+        }
+        restProducts.push(product);
+    }
+    return restProducts;
+}
+
+console.log(getRestProduct('phone'));
--- problem.js
+++ problem.js
@@ -1,20 +1,20 @@
 const porsons = [
-    {name : 'porsone1', age : 34},
-    {name : 'porsone2', age : 33},
-    {name : 'porsone3', age : 35},
-    {name : 'porsone4', age : 36},
-    {name : 'porsone5', age : 37},
-    {name : 'porsone6', age : 38},
-    {name : 'porsone7', age : 38},
-    {name : 'porsone8', age : 39}
+    { name: 'porsone1', age: 34 },
+    { name: 'porsone2', age: 33 },
+    { name: 'porsone3', age: 35 },
+    { name: 'porsone4', age: 36 },
+    { name: 'porsone5', age: 37 },
+    { name: 'porsone6', age: 38 },
+    { name: 'porsone7', age: 38 },
+    { name: 'porsone8', age: 39 }
 ]
 
 function objectAcces(arr) {
     let valus = [];
-    for (let porson of porsons){
+    for (let porson of porsons) {
         let text = "";
-        for (let property in porson){
-            text += property +': '+ porson[property]+ ' ';
+        for (let property in porson) {
+            text += property + ': ' + porson[property] + ' ';
         }
         // console.log(text);
         valus.push(text);
